43db7fbcd2861d88ea0af41350092f26380ebe2a
YuminJo <nationmilo@gmail.com>
2023-11-06 09:45:15 +0900

    Prepare Test Code Scripts

--- src/main/java/com/ll/base/App.java
+++ src/main/java/com/ll/base/App.java
@@ -1,10 +1,14 @@
 package com.ll.base;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
+	private Scanner scanner;
 	public App() {
+		scanner = new Scanner(System.in);
 	}
 
 	public void run() {
+		System.out.println("== 명언 앱 ==");
 	}
 }
---
+++ src/test/java/com/ll/AppTest.java
@@ -0,0 +1,31 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Scanner;
+
+public class AppTest extends TestUtil
+{
+	@Test
+	@DisplayName("종료를 입력하면 앱이 종료된다.")
+	void QuitApp()
+	{
+
+	}
+
+	// @Test
+	// void set_in_test() {
+	// 	systemIn("원하는 입력값");
+	// 	test();
+	//
+	// 	assertThat(getOutput()).contains("원하는 입력값");
+	// }
+	//
+	// void test() {
+	// 	Scanner scanner = new Scanner(System.in);
+	// 	System.out.println(scanner.nextLine());
+	// }
+}
